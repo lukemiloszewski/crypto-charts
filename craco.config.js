15e463cada97a1dfcfeda02cbba0cd22e02e473d
Luke Miloszewski <lukemiloszewski@gmail.com>
2021-12-04 10:47:51 +0200

    Directory structure is good

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components/"),
+      "@assets": path.resolve(__dirname, "./src/assets/"),
+      "@utils": path.resolve(__dirname, "./src/utils/"),
+    },
+  },
+};
