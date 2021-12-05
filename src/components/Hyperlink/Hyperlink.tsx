49faec2bcc5bd9a2ff7089379bef07f494b75640
Luke Miloszewski <lukemiloszewski@gmail.com>
2021-12-05 14:35:54 +0200

    Add vsix dependencies

--- src/components/Hyperlink/Hyperlink.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Hyperlink = styled.a`
-  color: #61dafb;
-  text-decoration: none;
-  padding-bottom: 0.5rem;
-  border-bottom: solid 0.25rem;
-  border-color: #61dafb;
-`;
