77eebf5ec5d6daa1873a51d9fb911c0c604f513a
Luke Miloszewski <lukemiloszewski@gmail.com>
2021-12-06 21:24:01 +0200

    Reduce font size

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -10,7 +10,7 @@ export const Paragraph = styled.p<Props>`
   color: ${(props) => (props.color ? props.color : "rgba(255, 255, 255, 0.6)")};
   font-weight: ${(props) => (props.isBold ? "bold" : 400)};
   font-style: ${(props) => (props.isItalic ? "italic" : "normal")};
-  font-size: 18px;
+  font-size: 16px;
   letter-spacing: 0.1px;
   line-height: 26px;
 `;
