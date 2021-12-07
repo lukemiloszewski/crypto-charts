647f736e8ad084e7846c8b3afb5aada55b01a090
Luke Miloszewski <lukemiloszewski@gmail.com>
2021-12-07 09:02:09 +0200

    Chart container

--- src/components/ChartContainer/ChartContainer.tsx
+++ src/components/ChartContainer/ChartContainer.tsx
@@ -1,9 +1,6 @@
 import styled from "styled-components";
 
 export const ChartContainer = styled.div`
-  text-align: left;
-  text-align: justify;
-  text-justify: inter-word;
   width: 1000px;
   max-width: 80%;
 `;
